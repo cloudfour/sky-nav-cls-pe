a1bb3de43394f405f408dd86bcb3e7e21f0309cc
Gerardo Rodriguez <mr.gerardo.rodriguez@gmail.com>
2022-08-26 08:32:03 -0700

    Playing with CSS inlining

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
+// import compress from 'astro-compress';
+// import critters from 'astro-critters';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// integrations: [critters()],
+});
